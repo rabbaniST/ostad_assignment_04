3ecfe2ea6d779c3a3739c800e9dd27eb5c6f0508
mdgolamrabbani <you@example.com>
2023-10-02 22:06:41 +0600

    First Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+class Product {
+    public $id;
+    public $name;
+    public $price;
+    
+    
+    public function __construct($id, $name, $price) {
+        $this -> id = $id;
+        $this -> name = $name;
+        $this -> price = $price;
+    }
+
+    public function getFormattedPrice(){
+        return '$'. number_format($this->price,2);
+    }
+
+    public function showDetails(){
+    echo " Product Details:\n ID:{$this->id}\n Name: {$this->name}\n Price: {$this->getFormattedPrice()}\n";
+
+    }
+}
+
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+
